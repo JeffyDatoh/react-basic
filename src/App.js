d11513ef3dc907a83f3781fa7d28b82d17206ea5
JeffyDatoh <jeffy.dth@gmail.com>
2022-12-18 13:43:40 +0700

    add new data

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ function App() {
       <MyBar />
       <h1 style={{paddingLeft: "10px"}}>Attraction</h1>
       <div className="grid-container">
-        <MyCard name={data[0].name} coverimage={data[0].coverimage} detail={data[0].detail} />
-        <MyCard name={data[1].name} coverimage={data[1].coverimage} detail={data[1].detail} />
-        <MyCard name={data[2].name} coverimage={data[2].coverimage} detail={data[2].detail} />
-        <MyCard name={data[3].name} coverimage={data[3].coverimage} detail={data[3].detail} />
-        <MyCard name={data[4].name} coverimage={data[4].coverimage} detail={data[4].detail} />
+        {data.map(item => (
+          <MyCard key={item.id} name={item.name} coverimage={item.coverimage} detail={item.detail} />
+        ))}
       </div>
     </>
   );
